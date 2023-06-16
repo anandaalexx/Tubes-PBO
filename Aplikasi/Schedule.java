5fd183e728e13edd7e640b393bd3ca180a8e692e
anandaalex <11221032@student.itk.ac.id>
2023-06-16 20:32:55 +0800

    Menambah isi file Schedule.java

--- Aplikasi/Schedule.java
+++ Aplikasi/Schedule.java
@@ -1,10 +1,16 @@
 package Aplikasi;
 
+import java.util.Date;
+
 public class Schedule {
     private String title;
+    private String prioritas;
+    private Date tanggal;
 
-    public Schedule(String title) {
+    public Schedule(String title, String prioritas, Date tanggal) {
         this.title = title;
+        this.prioritas = prioritas;
+        this.tanggal = tanggal;
     }
 
     public String getTitle() {
@@ -13,5 +19,21 @@ public class Schedule {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getPrioritas(){
+        return prioritas;
+    }
+
+    public void setPrioritas(String prioritas){
+        this.prioritas = prioritas;
+    }
+
+    public Date getTanggal(){
+        return tanggal;
+    }
+
+    public void setTanggal(Date tanggal){
+        this.tanggal = tanggal;
     }
 }
