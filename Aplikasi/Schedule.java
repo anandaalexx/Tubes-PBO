e870c3bd5246be22be765661b5882f23318849a7
anandaalex <11221032@student.itk.ac.id>
2023-06-16 20:26:22 +0800

    Menambahkan file Schedule.java

---
+++ Aplikasi/Schedule.java
@@ -0,0 +1,17 @@
+package Aplikasi;
+
+public class Schedule {
+    private String title;
+
+    public Schedule(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
